2c36c6780e9533bb1081d882ed3c631b930732f6
Lidiane Taquehara <lidi.mayra@gmail.com>
2019-07-28 19:01:19 -0300

    Create an Application class

--- src/main/java/com/tripper/Application.java
+++ src/main/java/com/tripper/Application.java
@@ -1,4 +1,34 @@
 package com.tripper;
 
+import com.tripper.entity.Event;
+import com.tripper.repository.EventRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Calendar;
+
+@SpringBootApplication
 public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    @Bean
+    public CommandLineRunner demo(EventRepository eventRepository) {
+        return (args) -> {
+            String description = "Lorem Ipsum";
+
+            eventRepository.save(new Event(Calendar.getInstance(), Calendar.getInstance(), "XPTO", description));
+
+            eventRepository.findByTitle("XPTO").forEach(e -> {
+                log.info(e.getDescription().toString());
+            });
+        };
+    }
 }
