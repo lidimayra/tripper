0c4df3e2c97212472e18d2c02f7226f59a425a6e
Lidiane Taquehara <lidi.mayra@gmail.com>
2019-08-25 18:53:19 -0300

    Use Thymeleaf to render HTML view
    Enable requests performed to "/events" to result in "events.html"
    page.

---
+++ src/main/java/com/tripper/EventsController.java
@@ -0,0 +1,12 @@
+package com.tripper;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EventsController {
+    @GetMapping("/events")
+    public String index() {
+        return "events";
+    }
+}
